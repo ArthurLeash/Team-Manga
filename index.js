6dec6acd79cf8f362e0f1d7c3407e019cf1a0895
ArthurLeash <45146043+ArthurLeash@users.noreply.github.com>
2018-11-29 16:26:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ bot.on(' ready' , function () {
 })
 
 
+
+bot.on(' ready' , function () {
+    console.log(" je suis prêt a être utilisé. ")
+    bot.user.setActivity("Regarder des Mangas").catch(console.error)
+});
+
+bot.on('guildMemberadd' , member => {
+    member.createDM().then(channel => {
+        return channel.send('bienvenue sur le serveur' + member.displayName)
+        console.log('$(member.displayName) a rejoint le serveur')
+    }).catch(console.error)
+});
+
+bot.on('message' , msg => {
+    if(msg.content === "bonjour")
+    msg.reply("Ohayô.")
+});
 bot.login(cfg);
