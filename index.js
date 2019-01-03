fa4e125d76b62fde9532e19dd5f89d6ad0f13884
ArthurLeash <45146043+ArthurLeash@users.noreply.github.com>
2019-01-03 18:48:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const bot = new Discord.Client()
-const cfg = process.env.token
-const prefix = ("/")
-
-bot.on('ready' , async () => {
+const cfg = require('./index.json')
+const giphy = require('giphy')('7XnyOQobDV75nO1D6VFdiYWZOPTPGhIJ')
+const ban = require('./bans/ban');
+ 
+let prefix = "/"
+ 
+bot.on('ready' , function () {
     console.log(" je suis prêt a être utilisé. ")
-    bot.user.setActivity("Regarder des Mangas")
+    bot.user.setActivity("Regarder des Mangas").catch(console.error)
 });
-
-bot.on('guildMemberAdd' , member => {
+ 
+bot.on('guildMemberadd' , member => {
     member.createDM().then(channel => {
         return channel.send('bienvenue sur le serveur' + member.displayName)
         console.log('$(member.displayName) a rejoint le serveur')
     }).catch(console.error)
 });
-
-
-const ban = require('./kick et ban/ban')
-
+ 
 bot.on('message', function (message){
-if (ban.match(message)){
-    return ban.action(message)
-}
-
-});
-
-bot.on('message' , msg => {
-    if(msg.content === "bonjour")
-    msg.reply("Ohayô.")
-});
-
-bot.on('message' , msg => {
-    if(msg.content === "/info")
-    msg.reply("je suis actuellement en codage par Arthur Leash")
+    if(message.match("/^\.ban .+$/")){
+        return ban.action(message)
+    }
+ 
+    if(msg.content === "bonjour"){
+        msg.reply("Ohayô.")
+    }
+ 
+    if(msg.content === ".hug"){
+        msg.reply("Trop kawaiii !", {
+            file: giphy.search('hug manga')
+        })
+    }
+ 
+    if(msg.content === ".info"){
+        msg.reply("je suis actuellement en codage par Arthur Leash")
+    }
+ 
+    if(msg.content === ".help"){
+        msg.reply("les commandes dispnibles sont:\n/info afin de donner des informations peu utile sur le bot")
+    }
 })
-
-bot.on('message' , msg => {
-    if(msg.content === "/help")
-    msg.reply("les commandes dispnibles sont: /info afin de donner des informations peu utile sur le bot")
-})
-
-bot.login(cfg);
+ 
+bot.login(cfg.token);
