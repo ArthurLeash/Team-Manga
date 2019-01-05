25f9ded6fd5cb5aa2fde09c166d7df367bbfb987
ArthurLeash <45146043+ArthurLeash@users.noreply.github.com>
2019-01-05 20:46:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ bot.on('message', function (msg){
  
     if(msg.content === ".help"){
         msg.reply("les commandes disponibles sont:\n.info afin de donner des informations peu utile sur le bot\n.hug @x afin de faire un calin a la personne mentionné")
- 
+    }
     if(msg.content === ".staff"){
         msg.reply("les commandes disponibles sont:\n.ban @xx#0000 afin de bannir la personne mentionner")
     }
