350473c2d85699baff9a549831e6cc5180ccabd5
ArthurLeash <45146043+ArthurLeash@users.noreply.github.com>
2019-01-15 20:41:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ bot.on('message', function (msg){
     }
     
     if(msg.content.startsWith('.fuck')){
+        if(!msg.channel.nsfw) return ("Pas ici \:warning: ")
         const args = msg.content.slice(1).split(/ +/)
         const command = args.shift().toLowerCase()
         let member = msg.mentions.members.first();
@@ -161,7 +162,7 @@ bot.on('message', function (msg){
     }
  
     if(msg.content === ".help"){
-        msg.reply("les commandes disponibles sont:\n.info afin de donner des informations peu utile sur le bot\n.hug @x afin de faire un calin a la personne mentionné\n.kiss afin de faire un bisous a la personne mentionnée\n.kill afin de tuer la personne mentionnée\n.fuck afin de faire un Fuck a la personne mentionner")
+        msg.reply("les commandes disponibles sont:\n.info afin de donner des informations peu utile sur le bot\n.hug @x afin de faire un calin a la personne mentionné\n.kiss afin de faire un bisous a la personne mentionnée\n.kill afin de tuer la personne mentionnée\n.fuck afin de faire un Fuck a la personne")
     }
  
     if(msg.content === ".staff"){
